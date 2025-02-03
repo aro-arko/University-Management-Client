10812072ef7101c53b5625a5bc7b74a12c1609ac
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-02-03 19:45:58 +0800

    semester registration successfull

--- src/constants/semester.ts
+++ src/constants/semester.ts
@@ -3,3 +3,8 @@ export const semesterOptions = [
   { value: "02", label: "Summer" },
   { value: "03", label: "Fall" },
 ];
+export const semesterStatus = ["UPCOMING", "ONGOING", "ENDED"];
+export const semesterStatusOptions = semesterStatus.map((item) => ({
+  value: item,
+  label: item,
+}));
