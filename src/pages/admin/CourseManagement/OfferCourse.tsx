bc032a16276c8bdac8e20dc338d6f696aa975b74
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-02-05 03:38:06 +0800

    watch hook to get data from the hover option

--- src/pages/admin/CourseManagement/OfferCourse.tsx
+++ src/pages/admin/CourseManagement/OfferCourse.tsx
@@ -1,9 +1,35 @@
-const OfferCourse = () => {
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import PHSelectWithWatch from "../../../components/form/PHSelectWithWatch";
+
+const OfferedCourse = () => {
+  const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
+
+  const academicFacultyOptions = academicFacultyData?.data?.map((item) => ({
+    value: item._id,
+    label: item.name,
+  }));
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>This is OfferCourse component</h1>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelectWithWatch
+            label="Academic Semester"
+            name="academicSemester"
+            options={academicFacultyOptions}
+          />
+          <PHInput name="test" label="Test" type="text" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
-export default OfferCourse;
+export default OfferedCourse;
